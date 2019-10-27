4ecbdb8c81aca0c62ffd5375e8eadc0ed779b7b5
Mattia Natali <matitalatina@gmail.com>
2019-10-27 17:01:21 +0100

    Added wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
-pub mod anagram;
+mod anagram;
+
+extern crate js_sys;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::collections::HashSet;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn get_anagram(phrase: &str, dictionary: &JsValue) -> JsValue {
+  let dict: Vec<String> = dictionary.into_serde().unwrap();
+  let dict_hash: HashSet<&str> = dict.iter().map(|w| &w[..]).collect();
+  let anagrams = anagram::Phrase::new(phrase).get_anagrams(&dict_hash);
+  JsValue::from_serde(&anagrams).unwrap()
+}
